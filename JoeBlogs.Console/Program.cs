139796d17cfa68ed56a689cf6df9fa3c822cd7f4
fschwiet <fschwiet@gmail.com>
2012-03-22 21:15:57 -0700

    rebuild console csproj to be .net 4ish

---
+++ JoeBlogs.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JoeBlogs.Console
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var commands = ManyConsole.ConsoleCommandDispatcher.FindCommandsInSameAssemblyAs(typeof(Program));
+            return ManyConsole.ConsoleCommandDispatcher.DispatchCommand(commands, args, System.Console.Out);
+        }
+    }
+}
