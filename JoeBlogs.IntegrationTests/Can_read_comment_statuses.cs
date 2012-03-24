67fb3901d7fb1a8b8160b6a8bda3a2761f4cae5a
fschwiet <fschwiet@gmail.com>
2012-03-24 16:35:46 -0700

    integration test project is now .net 4.0, adding stub for comment status test

---
+++ JoeBlogs.IntegrationTests/Can_read_comment_statuses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace JoeBlogs.IntegrationTests
+{
+    [TestFixture]
+    public class Can_read_comment_statuses
+    {
+        [Test]
+        public void Should__read_list_of_comments()
+        {
+            
+        }
+    }
+
+
+}
