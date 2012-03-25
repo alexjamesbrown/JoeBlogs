4b5f18aa607f482c85357e140d36f88dfd81c396
fschwiet <fschwiet@gmail.com>
2012-03-24 17:08:39 -0700

    can read list of comments

--- JoeBlogs.IntegrationTests/Can_read_comment_statuses.cs
+++ JoeBlogs.IntegrationTests/Can_read_comment_statuses.cs
@@ -35,7 +35,7 @@ namespace JoeBlogs.IntegrationTests
 
             var results = wordpressWrapper.GetCommentStatusList("1");
 
-            Assert.That(results, Is.EqualTo(new [] {"approved", "hello"}));
+            Assert.That(results, Is.EqualTo(new [] {"Unapproved", "Approved", "Spam"}));
         }
 
         public class FakeWordPressServer : NancyModule
