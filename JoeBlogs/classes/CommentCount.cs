9eaad65d4a1f0e6099caeb3c7455625d8f090980
Yoel Masih <yoel.masih@un-install.co.uk>
2013-04-15 14:00:38 +0200

    changed int to object as fix for issue #22

--- JoeBlogs/classes/CommentCount.cs
+++ JoeBlogs/classes/CommentCount.cs
@@ -6,21 +6,21 @@ namespace JoeBlogs
         /// <summary>
         /// The number of comments marked as approved.
         /// </summary>
-        public int Approved;
+        public object Approved;
 
         /// <summary>
         /// The number of comments awaiting moderation
         /// </summary>
-        public int AwaitingModeration;
+        public object AwaitingModeration;
 
         /// <summary>
         /// The number of comments marked as spam
         /// </summary>
-        public int Spam;
+        public object Spam;
 
         /// <summary>
         /// The total number of comments.
         /// </summary>
-        public int TotalComments;
+        public object TotalComments;
     }
 }
